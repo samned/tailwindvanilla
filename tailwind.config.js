b3ecc161ebdf03b6614ca9ff5cf807b8cc6fbfa3
samnedb <samuelabebayehu@gmail.com>
2022-02-11 00:46:16 -0800

    added sample pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,31 @@ module.exports = {
   content: ["./src/**/*.html"], //configure this line as you see fit
   theme: {
     extend: {
-      colors:{
+      colors: {
         "bookmark-purple": "#5267DF",
         "bookmark-red": "#FA5959",
         "bookmark-blue": "#242A45",
         "bookmark-grey": "#9194A2",
         "bookmark-white": "#f7f7f7",
-      }
+        "bright-yellow": "yellow",
+      },
     },
-    fontFamily:{
-      Poppins:["Poppins,sans-serif"]
+    fontFamily: {
+      Poppins: ["Poppins,sans-serif"],
     },
     container: {
       center: true,
-      padding:"1rem",
-      screens:{
-        lg:"1124px",
-        xl:"1124px",
-        "2xl":"1124px",
-      }
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+    borderWidth: {
+      DEFAULT: "1px",
+      '2': '0.2px',
     },
   },
   plugins: [],
-}
+};
